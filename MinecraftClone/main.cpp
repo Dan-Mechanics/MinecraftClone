9407c71df41465c095b7a0e875c0bd7a20e949f5
Dan-Mechanics <github.tim@desmidt.org>
2025-11-14 17:46:07 +0100

    doesnt work

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -4,6 +4,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+const char* vertexShaderSource = "#version 340 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 340 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 	if (!glfwInit())
 		return -1;
@@ -12,6 +27,14 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // THIS SHOULD BE 4, BUT THIS WORKS.
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
+	// WE ARE MAKING A TRIANGLE.
+	// LET'S FUCKING GOOOO !!
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // x 
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // y
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // z
+	};
+
 	const auto w = 800;
 	const auto h = 800; //									    FULLSCREEN
 	GLFWwindow* window = glfwCreateWindow(w, h, "Minecraft Clone", NULL, NULL);
@@ -28,14 +51,60 @@ int main() {
 
 	glViewport(0, 0, w, h);
 
-	// "NICE" BLUE.
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	// VERTEX BUFFER OBJECT.
+	GLuint vao, vbo;
+
+	glGenVertexArrays(1, &vao); // VAO BEFOER VBO
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// https://youtu.be/hYZNN0MTLuc?si=J-2sBjE6RF0D-9fs&t=748
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// BACKGROUND COLOR
 	glClearColor(0.07f, 0.12f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.07f, 0.12f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// https://youtu.be/hYZNN0MTLuc?si=37cYZOxjReZnOVRa&t=834
+		
+		glfwSwapBuffers(window);
+
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
