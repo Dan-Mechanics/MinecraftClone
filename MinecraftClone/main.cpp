6d6b8482d62da9e1ba0a52b941579ce934f633b4
Dan-Mechanics <github.tim@desmidt.org>
2025-11-14 17:09:49 +0100

    works

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -1,9 +1,32 @@
+// https://youtu.be/z03LXhRBLGI?si=kGOiI6dm8b5YW3Lo
+
 #include <iostream>
-#include "glad/glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main() {
-	std::cout << "Hello world" << std::endl;
-	std::cin.get();
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // this should be 4 but this works.
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	//									  w    h                     FULLSCREEN
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
