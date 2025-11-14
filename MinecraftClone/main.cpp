c6fade73ab8772b48cefb757a8691d59bb91865a
Dan-Mechanics <github.tim@desmidt.org>
2025-11-14 18:02:44 +0100

    remove comments

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -79,7 +79,6 @@ int main() {
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// https://youtu.be/hYZNN0MTLuc?si=J-2sBjE6RF0D-9fs&t=748
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -93,7 +92,6 @@ int main() {
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// https://youtu.be/hYZNN0MTLuc?si=37cYZOxjReZnOVRa&t=834
 		
 		glfwSwapBuffers(window);
 
