011d81acfbf63b1c080452d4171073eb837b0e7d
Dan-Mechanics <github.tim@desmidt.org>
2025-11-15 15:10:55 +0100

    spinning

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -5,26 +5,36 @@
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Texture.h"
 #include "shaderClass.h"
 #include "vbo.h"
 #include "vao.h"
 #include "ebo.h"
 
-// WE ARE MAKING A TRIANGLE.
-// LET'S FUCKING GOOOO !!
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+// WE ARE MAKING A CUBE.
 GLfloat vertices[] =
 { 
-	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, 
-	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, 
-	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, 
-	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  
+	-0.5f, 0.0f, 0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f, 
+	-0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,     5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,     0.0f, 0.0f,
+	 0.5f, 0.0f, 0.5f,     0.83f, 0.70f, 1.44f,     5.0f, 0.0f,
+	 0.0f, 0.8f, 0.0f,     0.92f, 0.86f, 0.76f,     2.5f, 5.0f  
 };
 
 GLuint indices[] =
 {
-	0, 2, 1, 
-	0, 3, 2 
+	0, 1, 2, 
+	0, 2, 3, 
+	0, 1, 4, 
+	1, 2, 4, 
+	2, 3, 4, 
+	3, 0, 4
 };
 
 int main() {
@@ -35,9 +45,8 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // THIS SHOULD BE 4, BUT THIS WORKS.
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	const auto w = 800;
-	const auto h = 800; //									    FULLSCREEN
-	GLFWwindow* window = glfwCreateWindow(w, h, "Minecraft Clone", NULL, NULL);
+	// HERE YOU CAN SET FULLSCREEN OR NOT.
+	GLFWwindow* window = glfwCreateWindow(width, height, "Minecraft Clone", NULL, NULL);
 
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -49,7 +58,7 @@ int main() {
 
 	gladLoadGL();
 
-	glViewport(0, 0, w, h);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram{ "default.vert", "default.frag" };
 
@@ -71,15 +80,42 @@ int main() {
 	Texture bluebells("texture.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	bluebells.setTextureUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
 	while (!glfwWindowShouldClose(window)) {
 		// BACKGROUND COLOR	
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		shaderProgram.activate();
+
+		double currentTime = glfwGetTime();
+		if (currentTime - prevTime >= 1.0f / 60) {
+			rotation += 0.5f;
+			prevTime = currentTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f); // UNITY CAM !!
+
+		int modelLocation = glGetUniformLocation(shaderProgram.id, "model");
+		glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+		int viewLocation = glGetUniformLocation(shaderProgram.id, "view");
+		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+
+		int projLocation = glGetUniformLocation(shaderProgram.id, "proj");
+		glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(proj));
+
 		glUniform1f(uniformId, 0.5f);
 		bluebells.bind();
 		vao1.bind(); //				 COUNT OF INDICES.
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
