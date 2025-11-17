9591bc322c21132c5f1bfd39a89e873ec0f4b58b
Dan-Mechanics <github.tim@desmidt.org>
2025-11-17 12:55:16 +0100

    working on it

---
+++ MinecraftClone/Mesh.h
@@ -0,0 +1,24 @@
+#ifndef MESH_CLASS_H
+#define MESH_CLASS_H
+
+#include <string>
+
+#include "vao.h"
+#include "vbo.h"
+#include "ebo.h"
+#include "Camera.h"
+#include "Texture.h"
+#include <vector>
+
+class Mesh {
+public:
+	// std::vector<Vertex> vertices;
+	// std::vector<GLuint> indices;
+	// std::vector<Texture> textures;
+	// 
+	// vao vao;
+	// Mesh(std::vector())
+};
+
+#endif
+
