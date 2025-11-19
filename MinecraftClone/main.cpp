c5f4d94e31db5af7b562e294fda7cb76c25b5ff8
Dan-Mechanics <github.tim@desmidt.org>
2025-11-19 21:16:05 +0100

    works

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -1,73 +1,101 @@
-// https://www.youtube.com/playlist?list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-
+#include"Model.h"
 
-#include "Model.h"
 
 const unsigned int width = 800;
 const unsigned int height = 800;
 
-int main() {
-	if (!glfwInit())
-		return -1;
 
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // THIS SHOULD BE 4, BUT THIS WORKS.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// HERE YOU CAN SET FULLSCREEN OR NOT.
-	GLFWwindow* window = glfwCreateWindow(width, height, "Minecraft Clone", NULL, NULL);
-
-	if (window == NULL) {
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
+	//Load GLAD so it configures OpenGL
 	gladLoadGL();
-
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
 
-	/// ===
 
+
+
+
+	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
 
-	// ===
-
-	glm::vec4 lightColor = glm::vec4{ 1.0f, 1.0f, 1.0f, 1.0f };
-	glm::vec3 lightPos = glm::vec3{ 0.5f, 0.5f, 0.5f };
-	glm::mat4 lightModel = glm::mat4{ 1.0f };
+	// Take care of all the light related things
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
 	lightModel = glm::translate(lightModel, lightPos);
 
 	shaderProgram.activate();
 	glUniform4f(glGetUniformLocation(shaderProgram.id, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.id, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
+
+
+
+
+	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
 
-	Camera camera{ width, height, glm::vec3{0.0f, 0.0f, 2.0f} };
+	// Creates camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-	Model model{ "models/sword/scene.gltf" };
 
-	while (!glfwWindowShouldClose(window)) {
-		// BACKGROUND COLOR	
-		//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClearColor(0.1f, 0.12f, 0.18f, 1.0f);
+	// Original code from the tutorial
+	Model model("models/sword/scene.gltf");
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// Handles camera inputs
 		camera.moveCamera(window);
+		// Updates and exports the camera matrix to the Vertex Shader
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
+		// Draw a model
 		model.draw(shaderProgram, camera);
 
+		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-	shaderProgram.free();
 
+
+	// Delete all the objects we've created
+	shaderProgram.free();
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
