9aa334c29a21962b03eff9547562f37a99623adb
Dan-Mechanics <github.tim@desmidt.org>
2025-12-03 19:53:11 +0100

    working on it

--- MinecraftClone/mesh_utils.h
+++ MinecraftClone/mesh_utils.h
@@ -1,11 +1,13 @@
 #pragma once
-#include <vector>
-#include "vbo.h"
-#include <unordered_set>
 #include "BlockPos.h"
+#include <unordered_set>
+#include "vbo.h"
+#include <unordered_map>
 #include "BlockType.h"
 #include "Direction.h"
-#include <unordered_map>
+
+constexpr auto CHUNK_SIZE_FLOAT = 16.0f;
+constexpr auto CHUNK_SIZE_INT = 16;
 
 extern const BlockPos UP;
 extern const BlockPos DOWN;
@@ -14,28 +16,33 @@ extern const BlockPos RIGHT;
 extern const BlockPos FORWARD;
 extern const BlockPos BACK;
 
-extern const std::unordered_map<BlockType, std::vector<glm::vec2>> map;
+void getChunk(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix,
+	const std::unordered_map<BlockType, std::vector<std::vector<glm::vec2>>>& atlas,
+	const std::unordered_map<BlockPos, BlockType>& chunk,
+	const std::unordered_map<BlockPos, std::unordered_map<BlockPos, BlockType>>& allChunks);
 
-void getCube(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix);
-void getChunk(const std::unordered_set<BlockPos>& blocks, std::vector<Vertex>& verts,
+bool has(const BlockPos& blockPos, const std::unordered_map<BlockPos, std::unordered_map<BlockPos, BlockType>>& allChunks);
+
+// REMOVE LATER   ----->
+
+void getNyceliumChunk(const std::unordered_set<BlockPos>& blocks, std::vector<Vertex>& verts,
 	std::vector<GLuint>& tris, glm::mat4& modelMatrix);
 
+void writeAllAroundFaceUVs(std::vector<Vertex>& verts);
 std::vector<glm::vec2> tilePositionToUvs(const int x, const int y);
 
-/// <summary>
-/// https://stackoverflow.com/questions/40776703/converting-from-radians-to-degrees
-/// </summary>
-int randomInclusive(const int min, const int max);
-
-/// <summary>
-/// Given that they are always the same.
-/// </summary>
-void writeAllAroundFaceUVs(std::vector<Vertex>& verts);
+// END REMOVE LATER < ------
 
 /// <summary>
 /// Rule: this can only be called after 4 verts have been added.
 /// </summary>
 void setCurrentFaceUvs(std::vector<Vertex>& verts, const std::vector<glm::vec2>& uvs);
+
+/// <summary>
+/// I hope this works.
+/// It should work because rounding lore.
+/// </summary>
+BlockPos blockPosToChunkPos(const BlockPos& blockPos);
 
 void getPyramid(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix);
 void setAllUvs(const std::vector<glm::vec2>& uvs, std::vector<std::vector<glm::vec2>>& allUvs);
--- MinecraftClone/utils.h
+++ MinecraftClone/utils.h
@@ -1,44 +1,13 @@
 #pragma once
+#include <stdlib.h>
+//#include <iostream>
 #include <vector>
 #include "vbo.h"
-#include <unordered_set>
-#include "BlockPos.h"
-#include "BlockType.h"
-#include "Direction.h"
-#include <unordered_map>
-
-extern const BlockPos UP;
-extern const BlockPos DOWN;
-extern const BlockPos LEFT;
-extern const BlockPos RIGHT;
-extern const BlockPos FORWARD;
-extern const BlockPos BACK;
-
-extern const std::unordered_map<BlockType, std::vector<glm::vec2>> map;
-
-void getCube(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix);
-void getChunk(const std::unordered_set<BlockPos>& blocks, std::vector<Vertex>& verts,
-	std::vector<GLuint>& tris, glm::mat4& modelMatrix);
-
-std::vector<glm::vec2> tilePositionToUvs(const int x, const int y);
 
 /// <summary>
 /// https://stackoverflow.com/questions/40776703/converting-from-radians-to-degrees
 /// </summary>
 int randomInclusive(const int min, const int max);
 
-/// <summary>
-/// Given that they are always the same.
-/// </summary>
-void writeAllAroundFaceUVs(std::vector<Vertex>& verts);
-
-/// <summary>
-/// Rule: this can only be called after 4 verts have been added.
-/// </summary>
-void setCurrentFaceUvs(std::vector<Vertex>& verts, const std::vector<glm::vec2>& uvs);
-
+void getCube(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix);
 void getPyramid(std::vector<Vertex>& verts, std::vector<GLuint>& tris, glm::mat4& modelMatrix);
-void setAllUvs(const std::vector<glm::vec2>& uvs, std::vector<std::vector<glm::vec2>>& allUvs);
-void setEquatorUvs(const std::vector<glm::vec2>& uvs, std::vector<std::vector<glm::vec2>>& allUvs);
-std::vector<std::vector<glm::vec2>> generateFillUvs();
-std::unordered_map<BlockType, std::vector<std::vector<glm::vec2>>> generateAtlas();
