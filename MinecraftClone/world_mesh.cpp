f578919eb810ad6bb01b11db770652fe3ee11d81
Dan-Mechanics <github.tim@desmidt.org>
2025-12-05 13:04:00 +0100

    changed hash function

--- MinecraftClone/world_mesh.cpp
+++ MinecraftClone/world_mesh.cpp
@@ -218,7 +218,7 @@ WORLD generateDemoWorld() {
 		chunk[{0, i * 2, 0}] = static_cast<BlockType>(i);
 	}*/
 
-	for (int x = 0; x < 16; ++x) {
+	for (int x = -3; x < 16; ++x) {
 		for (int y = 0; y < 16; ++y) {
 			for (int z = 0; z < 16; ++z) {
 				if (randomInclusive(0, 1))
