dab6a585ede6108f5884313eed6194a65ab8ad7f
Dan-Mechanics <github.tim@desmidt.org>
2025-12-07 21:03:12 +0100

    implemented soft shadows

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -167,7 +167,7 @@ int main() {
 
 	// DISABLE VSYNC.
 	glfwSwapInterval(0);
-	ShadowMapFBO shadowMap{ 2048, 2048, 20.0f };
+	ShadowMapFBO shadowMap{ 2048, 2048, 25.0f };
 
 	while (!glfwWindowShouldClose(window)) {
 		currentTime = (float)glfwGetTime();
