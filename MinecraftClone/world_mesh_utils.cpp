a6de2385e4d14839dead5c23a6ecef2e80eedcc7
Dan-Mechanics <github.tim@desmidt.org>
2025-12-12 13:16:02 +0100

    Revert "working on ivec3 shader"
    This reverts commit 1008e177ea312d03fbc203f9809ff4ee4d085074.

--- MinecraftClone/world_mesh_utils.cpp
+++ MinecraftClone/world_mesh_utils.cpp
@@ -155,6 +155,13 @@ void setCurrentFaceUVs(std::vector<Vertex>& verts, const Face& face) {
 		verts[beginFaceVert + i].texUv = face.uvs[i];
 		verts[beginFaceVert + i].texUv = face.uvs[i];
 		verts[beginFaceVert + i].texUv = face.uvs[i];
+
+		// MAKE THE VERTEX COLOR MAGENTA FOR DEBUG.
+		// WE WON'T SEE THIS BECAUSE OF THE TEXTURE.
+		verts[beginFaceVert + i].color = magenta;
+		verts[beginFaceVert + i].color = magenta;
+		verts[beginFaceVert + i].color = magenta;
+		verts[beginFaceVert + i].color = magenta;
 	}
 }
 
