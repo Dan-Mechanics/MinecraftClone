0ca7b77eac4fd61ae351bee496d7338c8a2fcbe5
Dan-Mechanics <github.tim@desmidt.org>
2025-12-18 15:59:17 +0100

    Update utils.h

--- MinecraftClone/utils.h
+++ MinecraftClone/utils.h
@@ -7,9 +7,11 @@
 #include "Texture.h"
 #include <string>
 
+// TODO: ADD NAMESPACE HERE.
+
 int randomInclusive(const int min, const int max);
 void logVec3(const glm::vec3& vec);
-void LogIvec3(const glm::ivec3& vec);
+void logIvec3(const glm::ivec3& vec);
 std::string ivec3ToString(const glm::ivec3& vec);
 void freeMaterial(const std::vector<Texture>& material);
 void bindMaterial(std::vector<Texture>& material, const Shader& shader);
