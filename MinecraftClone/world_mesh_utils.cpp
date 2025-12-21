9cb77ac452e9ca927c8dc440712fef71b5903b9c
Dan-Mechanics <github.tim@desmidt.org>
2025-12-21 14:42:36 +0100

    tree map

--- MinecraftClone/world_mesh_utils.cpp
+++ MinecraftClone/world_mesh_utils.cpp
@@ -171,6 +171,12 @@ Atlas generateAtlas() {
 	atlas.maps[BlockType::LOG].faces[Direction::UP] = tilePositionToUVs(3, 1);
 	atlas.maps[BlockType::LOG].faces[Direction::DOWN] = tilePositionToUVs(3, 1);
 
+	// GHOST LOG. ===
+	atlas.maps[BlockType::GHOST_LOG] = generateEmptyMap();
+	setEquatorUVs(atlas.maps[BlockType::GHOST_LOG], tilePositionToUVs(4, 0));
+	atlas.maps[BlockType::GHOST_LOG].faces[Direction::UP] = tilePositionToUVs(4, 1);
+	atlas.maps[BlockType::GHOST_LOG].faces[Direction::DOWN] = tilePositionToUVs(4, 1);
+
 	// ===
 	atlas.maps[BlockType::DIRT] = generateUniformMap(tilePositionToUVs(0, 1));
 	atlas.maps[BlockType::GRAVEL] = generateUniformMap(tilePositionToUVs(1, 1));
