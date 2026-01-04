a7d22407cb2d5db1746de8c6a9ad120d4c3c20ba
Dan-Mechanics <github.tim@desmidt.org>
2026-01-04 20:12:45 +0100

    Update main.cpp

--- MinecraftClone/Core/main.cpp
+++ MinecraftClone/Core/main.cpp
@@ -87,7 +87,7 @@ int main() {
 
 	// ===
 
-	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_DEPTH_TEST);
 
 	// https://learnopengl.com/Advanced-OpenGL/Face-culling
 	glEnable(GL_CULL_FACE);
@@ -134,11 +134,9 @@ int main() {
 		game.draw();
 
 		// https://github.com/VictorGordan/opengl-tutorials/blob/main/YoutubeOpenGL%2017%20-%20Transparency%20%26%20Blending/Main.cpp
-		glDisable(GL_CULL_FACE);
 		glEnable(GL_BLEND);
 		game.drawTranslucent();
 		glDisable(GL_BLEND);
-		glEnable(GL_CULL_FACE);
 
 		// UI. ===
 		glDisable(GL_DEPTH_TEST);
